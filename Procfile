33ccae704a5ae2a6bd438dbb0b8369be78abbfba
hollo <m.holloway@imperial.ac.uk>
2024-11-01 17:06:31 +0000

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar ./webapp-runner-10.1.31.0.jar --port 8080 ROOT.war
---
+++ src/main/java/MyApplet.java
@@ -0,0 +1,17 @@
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
+public class MyApplet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hello");
+        resp.setContentType("text/html");
+        resp.getWriter().write("Hello world second server");
+    }
+}
