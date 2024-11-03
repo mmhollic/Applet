7488bacf4c1e8dfb1d6bb9cdd7acb773a3f11508
hollo <m.holloway@imperial.ac.uk>
2024-11-03 14:15:33 +0000

    FIrst tsuru try on master

--- src/main/java/MyApplet.java
+++ src/main/java/MyApplet.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 @WebServlet(urlPatterns={"/patients","/doctors"},loadOnStartup = 1)
 public class MyApplet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("hello");
         resp.setContentType("text/html");
         resp.getWriter().write("Hello world Tsuru attempts");
     }
