28483cfbd0de915d25e1a3ac31de8419785ed287
hollo <m.holloway@imperial.ac.uk>
2024-11-04 13:43:11 +0000

    Latest tsuru try on master with test

--- src/test/java/TestServlet.java
+++ src/test/java/TestServlet.java
@@ -7,18 +7,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class TestServlet {
-
-    @Test
-    public void testScan()  {
-        int a=4, b=6;
-        assertEquals(a,b);
+    @Mock
+    HttpServletRequest request;
+    @Mock
+    HttpServletResponse response;
+    @BeforeEach
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
     }
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+        StringWriter stringWriter=new StringWriter();
+        PrintWriter printWriter=new PrintWriter(stringWriter);
 
+        when(response.getWriter()).thenReturn(printWriter);
+        when(request.getServletPath()).thenReturn("/patients");
+
+        MyApplet myServlet=new MyApplet();
+        myServlet.doGet(request,response);
+
+        String output=stringWriter.getBuffer().toString();
+        assertThat(output,is(equalTo("Hello, world!")));
+    }
 }
+
+
+
